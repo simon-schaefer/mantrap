d96a0b2396d34e29f7d8417c4cf753ebb97379c2
Simon Schaefer <sischaef@student.ethz.ch>
2019-10-07 10:38:37 -0700

    moved setup, blackify, scratchbook

---
+++ ops/blackify.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PROJECT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
+OPTIONS="-l 120"
+
+echo "Reformatting src files ..."
+black "$PROJECT_HOME"/murseco/*.py "$OPTIONS"
+for dir in "$PROJECT_HOME"/murseco/*/
+do
+  if [[ -n "$(ls "$dir"/*.py)" ]]
+  then
+    black "$dir"/*.py "$OPTIONS"
+  fi
+done
+
+echo "Reformatting test files ..."
+cd "$PROJECT_HOME"/test || return
+black *.py "$OPTIONS"
--- ops/setup.bash
+++ ops/setup.bash
@@ -2,25 +2,27 @@
 
 # Setup environment variables.
 PROJECT_OPS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-# PROJECT_NAME="$( dirname "${PROJECT_OPS}" )"
 PROJECT_HOME="$PROJECT_OPS/.."
+VIRTUAL_ENV=".venv_muresco"
 echo 'Setting up project ...'
 
 # Login to virtual environment.
 cd "${PROJECT_HOME}" || return
-if [[ ! -d 'venv' ]]; then
+if [[ ! -d "${VIRTUAL_ENV}" ]]; then
     echo 'Creating virtual environment ...'
-    mkdir venv
-    virtualenv -p python3 venv
+    mkdir "${VIRTUAL_ENV}"
+    virtualenv -p python3 "${VIRTUAL_ENV}"
 fi
 # shellcheck source=venv/bin/activate
-source "${PROJECT_HOME}"/venv/bin/activate
+source "${PROJECT_HOME}"/"${VIRTUAL_ENV}"/bin/activate
 
 # Install self-python-package.
 echo $'\nInstalling package ...'
 cd "${PROJECT_HOME}" || return
-pip3 install -r requirements.txt
+pip3 install -r ops/requirements.txt
+cp "${PROJECT_OPS}"/setup.py "${PROJECT_HOME}"
 pip3 install -e .
+rm "${PROJECT_HOME}"/setup.py
 
 cd "${PROJECT_HOME}" || return
 echo $'\nSuccessfully set up project !'
