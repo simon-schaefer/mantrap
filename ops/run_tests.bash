03cdac4c0d659cc22a50a0721202772d4b9858fc
Simon Schaefer <sischaef@student.ethz.ch>
2019-11-14 16:36:20 -0800

    initial julia

--- ops/run_tests.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PROJECT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
-# shellcheck source=.venv_muresco/bin/activate
-source "${PROJECT_HOME}"/.venv_muresco/bin/activate
-
-rm -r "${PROJECT_HOME}"/test/cache
-mkdir "${PROJECT_HOME}"/test/cache
-pytest "${PROJECT_HOME}"/test/*.py -vv
