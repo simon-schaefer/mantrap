c093f3207ee7105ef53b2c589ea59d5328b05c36
Simon Schaefer <sischaef@student.ethz.ch>
2020-04-16 16:14:00 +0200

    evaluation update (possible configurations, deleted runtime_analysis.py and evaluation.py since outdated)

---
+++ mantrap_evaluation/configurations.py
@@ -0,0 +1,15 @@
+"""
+Environment:
+- environment types
+- initial conditions
+- simulation time-step
+
+Solver
+- solver types
+- environment types
+- evaluation environment types
+- planning horizon: greed (T=1), medium (T=1s), long (T=2s)
+- objectives: (goal, interaction), goal only, interaction only
+- constraints: (max_speed, min_distance), max_speed only
+- filter: no_filter, uni-modal, attention radius
+"""
