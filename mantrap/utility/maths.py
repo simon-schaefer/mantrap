minor improvements (Derivative 2D fixed)

mantrap/utility/maths.py
@@ -131,9 +131,9 @@ class Derivative2:
 
         else:  # acceleration estimate based on velocities
             self._diff_mat = torch.zeros((horizon, horizon))
-            for k in range(1, horizon):
-                self._diff_mat[k, k - 1] = -1
-                self._diff_mat[k, k] = 1
+            for k in range(horizon - 1):
+                self._diff_mat[k, k] = -1
+                self._diff_mat[k, k + 1] = 1
             self._diff_mat *= 1 / dt
 
         # Un-squeeze difference matrix in case the state tensor is larger then two dimensional (batching).
@@ -142,9 +142,10 @@ class Derivative2:
 
     def compute(self, x: torch.Tensor) -> torch.Tensor:
         if len(x.shape) == 3:
-            return torch.matmul(self._diff_mat, torch.transpose(x, 0, 1))
+            x_compute = torch.transpose(x, 0, 1)
         else:
-            return torch.matmul(self._diff_mat, x)
+            x_compute = x
+        return torch.matmul(self._diff_mat, x_compute)
 
     def compute_single(self, x: torch.Tensor, x_prev: torch.Tensor, x_next: torch.Tensor) -> torch.Tensor:
         return (x_prev - 2 * x + x_next) / self._dt**2

test/test_utility.py
@@ -55,8 +55,8 @@ def test_derivative_2_velocity():
     diff = mantrap.utility.maths.Derivative2(horizon=2, dt=1.0, velocity=True)
     x_ddt = diff.compute(x[:, 2:4])
 
-    assert torch.all(torch.isclose(x_ddt[0, :], torch.zeros(2)))
-    assert torch.all(torch.isclose(x_ddt[1, :], (x[1, 2:4] - x[0, 2:4]) / diff._dt))
+    assert torch.all(torch.isclose(x_ddt[-1, :], torch.zeros(2)))
+    assert torch.all(torch.isclose(x_ddt[0, :], (x[1, 2:4] - x[0, 2:4]) / diff._dt))
 
 
 ###########################################################################